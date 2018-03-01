b37b64c334275b7defbff19f7f6b4522dd72b3f4
ricmado <ricmado@gmail.com>
2018-03-01 17:17:47 +0100

    Update argumentspython.py

--- Documents/GIT/Ficosa Python/argumentspython.py
+++ Documents/GIT/Ficosa Python/argumentspython.py
@@ -4,4 +4,4 @@ import sys
 
 print 'Number of arguments:', len(sys.argv), 'arguments.'
 print 'Argument List:', str(sys.argv)
-print 'Solo el argumento 1', sys.argv[1]
+print 'Solo el argumento 1:', sys.argv[1]
