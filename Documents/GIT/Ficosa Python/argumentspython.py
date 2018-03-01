3f3cbda97bd9fae2f79cb41feb719e50e37e0fc8
ricmado <ricmado@gmail.com>
2018-03-01 17:58:15 +0100

    Update argumentspython.py

--- Documents/GIT/Ficosa Python/argumentspython.py
+++ Documents/GIT/Ficosa Python/argumentspython.py
@@ -6,6 +6,7 @@ import os
 print 'Number of arguments:', len(sys.argv), 'arguments.'
 print 'Argument List:', str(sys.argv)
 print 'Solo el argumento 1:', sys.argv[1]
-newpath = r'C:\Users\Administrator\Documents\FolderTest\desdepython' 
+newpath = sys.argv[1]+ desdepython
+newpathtwo = r'C:\Users\Administrator\Documents\FolderTest\desdepython' 
 if not os.path.exists(newpath):
     os.makedirs(newpath)
