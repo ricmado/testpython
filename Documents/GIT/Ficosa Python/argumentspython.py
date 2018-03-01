72cb7bb485ad479a617f6ae5a47c09a830fc6479
ricmado <ricmado@gmail.com>
2018-03-01 17:50:39 +0100

    Update argumentspython.py

--- Documents/GIT/Ficosa Python/argumentspython.py
+++ Documents/GIT/Ficosa Python/argumentspython.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python
 
 import sys
+import os
 
 print 'Number of arguments:', len(sys.argv), 'arguments.'
 print 'Argument List:', str(sys.argv)
 print 'Solo el argumento 1:', sys.argv[1]
+if not os.path.exists(sys.argv[1]):
+    os.makedirs(sys.argv[1]/desdepython)
