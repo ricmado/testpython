8093c70388044c3cf439e266516bb234a983237d
ricmado <ricmado@gmail.com>
2018-03-02 11:05:16 +0100

    new repo for the same files different path

---
+++ Documents/GIT/fico_python/pythons/ficosa_python/argumentspython.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+import sys
+import os
+
+print 'Number of arguments:', len(sys.argv), 'arguments.'
+print 'Argument List:', str(sys.argv)
+print 'Solo el argumento 1:', sys.argv[1]
+newpath = sys.argv[1]+ '/desdepython'
+print 'Junto:', newpath
+newpathtwo = r'C:\Users\Administrator\Documents\FolderTest\desdepython' 
+if not os.path.exists(newpath):
+    os.makedirs(newpath)
